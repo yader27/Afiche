e9b3af9b5e07e53e24c1487c5cc1d21414e40552
YDR27 <127981537+YDR27@users.noreply.github.com>
2024-11-23 17:42:28 -0600

    feat(all files): Add lottie animations and changes in responsive desing

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-window.onload = function() {
+window.onload = function () {
     // Mostrar el logo y la imagen de fondo, luego de 2 segundos, mostrar "Presenta"
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('.main-title-image').style.display = 'none'
         document.querySelector('.presenta-text').style.display = 'block';
     }, 400);
 
     // Después de 4 segundos, mostrar el resto de contenido
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('.main-title-image').style.display = 'block';
         document.querySelector('.presenta-text').style.display = 'none';
         // Agrega cualquier otra animación si lo deseas
-        document.querySelector('.main-title-image').classList.add('animate__fadeIn'); 
-    }, 2000);
+        document.querySelector('.main-title-image').classList.add('animate__fadeIn');
+    }, 4000);
+
+    // Selecciona el contenedor
+    const snowAnimation = document.getElementById('snow-animation');
+
+    // Carga la animación usando Lottie
+    lottie.loadAnimation({
+        container: snowAnimation, // Contenedor donde se reproducirá
+        renderer: 'svg', // Usa el renderizador SVG para mejor calidad
+        loop: true, // La animación se repite indefinidamente
+        autoplay: true, // La animación inicia automáticamente
+        path: 'https://lottie.host/7239407f-fdcd-4120-8796-d9691ac376f2/ukL0NAraEo.json' // Ruta al archivo JSON de la animación
+    });
 }
